1e6e56c7f093ce855f3987626a7b15dad1d16e9a
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-17 13:27:50 +0200

    C - Hello, World

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	double doubleType;
 	char charType;
 
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
-	printf("Size of char: %zu bytes\n", sizeof(charType));
+	printf("Size of int: %u bytes\n", sizeof(intType));
+	printf("Size of float: %u bytes\n", sizeof(floatType));
+	printf("Size of double: %u bytes\n", sizeof(doubleType));
+	printf("Size of char: %u bytes\n", sizeof(charType));
 
 	return (0);
 
