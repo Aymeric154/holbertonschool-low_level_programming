ac471ebbfc77db20fe550d7ee845256fd2b9f79b
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-17 12:34:47 +0200

    C - Hello, World

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  main - Puts Programming is like building a multilingual
- *  puzzle, followed by a new line.
- *  Return : alway 0
+ * main - Puts "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
  *
+ * Return: Always 0.
  */
 
 int main(void)
