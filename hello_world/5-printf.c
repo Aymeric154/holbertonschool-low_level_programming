2cbb6674c3614f3e5ba7f7580005fc9cfd0d34f8
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-17 11:24:11 +0200

    C - Hello, World

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-	int main(void)
-	{
-		printf("with proper grammar, but the outcome is a piece of art");
-		
-		return (0);
-	}
+        int main(void)
+        {
+                printf("Programming is like building a multilingual puzzle \n");
+
+                return (0);
+        }
