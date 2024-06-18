c3f3b735c2c11966a38a54bc4848f228fdddc66e
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-18 09:49:23 +0200

    C - Variables, if, else, while

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	char start = "a";
+	char end = "z";
+
+	char alphabet = alphabetStart;
+
+	while(start != end)
+	{
+		putchar(start);
+		start++
+	}
+
+	return (0);
+}
