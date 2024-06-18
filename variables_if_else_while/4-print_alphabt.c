1ba35ffdf39f522a77353e160083609929562cac
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-18 11:00:20 +0200

    C - Variables, if, else, while

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -  prints the alphabet in lowercase
+ * except q and e
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letters
+
+		for (letters = 'a' ; letters = 'z' ; letters++)
+		{
+			if (letters != 'q' && letters != 'e')
+			putchar (letters);
+		}
+
+	putchar('\n');
+
+	return (0);
+
+}
