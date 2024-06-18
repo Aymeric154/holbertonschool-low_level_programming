ffb4b5962a9e815c0d6240c4a478a715f2fa79b8
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-18 12:56:50 +0200

    C - Variables, if, else, while

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  program that prints the lowercase alphabet in reverse
+ *
+ * Return:  alway zero
+ */
+
+int main(void)
+{
+	char apbt;
+
+	for (apbt = 'z' ; apbt <= 'a'; apbt++)
+	{
+		putchar(apbt);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
