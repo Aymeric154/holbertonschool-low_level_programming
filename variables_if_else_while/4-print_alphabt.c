6013b146a62c498fc34e16cde3705224dc775557
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-18 11:13:41 +0200

    C - Variables, if, else, while

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -12,13 +12,13 @@ int main(void)
 {
 	char letters;
 
-		for(letters = 'a' ; letters = 'z' ; letters++)
-		{
+	for (letters = 'a' ; letters = 'z' ; letters++)
+	{
 
-			if(letters != 'e' && letters != 'q')
-				putchar (letters);
+		if (letters != 'e' && letters != 'q')
+			putchar (letters);
 
-		}
+	}
 
 	putchar('\n');
 
