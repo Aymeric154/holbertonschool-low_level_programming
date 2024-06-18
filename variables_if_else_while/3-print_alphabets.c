1b85c9daa7dd81f014ba7108097e6dfc1c4b5ae9
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-18 10:22:42 +0200

    C - Variables, if, else, while

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -  program that prints the alphabet in lowercase,
+ * and then in uppercase
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char alpb;
+
+	for( alpb = 'A' ; alpb <= 'B' ; alpb++)
+	{
+		putchar(alpb);
+	}
+
+	for( alpb ='a' ; alpb <= 'b' ; alpb++)
+	{
+		putchar(alpb);
+	}
+
+	putchar('\n');
+
+	return(0);
+}
