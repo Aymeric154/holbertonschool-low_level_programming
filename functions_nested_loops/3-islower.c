cf77f67ccda227881d7e2d474334894dfd246b4e
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-19 11:43:55 +0200

    C - Functions, nested loops

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * main  -  function that checks for lowercase character.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		islower(63);
+		_putchar(c);
+	}
+
+}
