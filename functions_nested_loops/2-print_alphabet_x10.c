93c47e4ba7079f278c30f6864fca6d83c1fedd81
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-19 11:09:32 +0200

    C - Functions, nested loops

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that prints 10 times the alphabet,
+ *  in lowercase
+ *
+ *  Return: we don't use return
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a' ; c = 'z' ; c++)
+	{
+
+		if (c < 10)
+		{	
+			_putchar(c);
+		}
+	}
+
+	_putchar('\n');
+
+}
