d0dbe1f88656931787b45bf4753528b32fb13908
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-19 15:34:34 +0200

    C - Functions, nested loops

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number.
+ * @x : number to check
+ *
+ * Result: 0.
+ */
+
+int print_last_digit(int x)
+{
+	if (x % 10)
+	{
+		return (x);
+	}
+}
