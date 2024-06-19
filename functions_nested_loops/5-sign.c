c815f811cf27169ba075cd4b7ecf2d1fa0522686
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-19 13:57:06 +0200

    C - Functions, nested loops

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *
+ * Return: zero
+ */
+
+int print_sign(int n)
+{
+	if ( n > 0)
+	{
+		_putchar("2B");
+		return (1);
+	}
+       	if else ( n < 0)
+	{
+		_putchar("2D");
+		return (-1);
+	}
+	else
+       	{
+		_putchar("30");
+		return (0);
+	}
+}
+
+
