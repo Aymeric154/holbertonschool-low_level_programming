f8a29fc1d6c441409c39dbdde31bb2c0172506e7
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-20 13:21:59 +0200

    C - Functions, nested loops

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,18 +8,31 @@
 
 void jack_bauer(void)
 {
-	int hour, minute = 0;
+	int hour, h, minute, m = 0;
 
-	while (hour < 24)
+	while (hour < 2)
 	{
-		while ( minute < 60)
+		hour++
+		while ( h < 9 )
 		{
-		_putchar(hour + '0');
-		_putchar(hour + '0');
-		_putchar(':');
-		_putchar(minute + '0');
-		_putchar(minute + '0');
-		return(0);
+			h++
+			if ((hour < 1 && h < 9) || (hour < 2 && h < 3))
+			{
+				while (minute < 5)
+				{
+					minute++
+						while (m < 9)
+						{
+							m++
+							_putchar(hour + '0');
+							_putchar(h + '0');
+							_putchar(':');
+							_putchar(minute + '0');
+							_putchar(m + '0');
+						}
+				}
+			}
 		}
+	}
 
 }
