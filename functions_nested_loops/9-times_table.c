8b84c0e60cb91edf1cf31a471893c967077c0573
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-20 21:52:49 +0200

    C - Functions, nested loops

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,16 +10,32 @@ void times_table(void)
 {
 	int n, m, o;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 0; n < 10; n++)
 	{
-		for (m = 1 ; m <= 9; m++)
+		for (m = 0 ; m < 10; m++)
 		{
-			_putchar('0');
-			_putchar(',');
-			_putchar(' ');
-
 			o = n * m;
-			
+
+			if (m == 0)
+			{
+				_putchar(o + '0');
+			}
+
+			if ( n < 10 && m != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(o + '0');
+			} else if (o >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + o / 10);
+				_putchar('0' + o % 10);
+			}
 		}
+		_putchar('\n');
+
 	}
 }
