985707908bb04c91148d26408f8bd4a2fec8589d
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-21 10:01:46 +0200

    C - Functions, nested loops

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,14 +2,14 @@
 
 /**
  * add -  adds two integers and returns the result.
+ * @number1: is integer
+ * @number2: is integer
  *
  * Return: alway zero
  */
 
 int add(int number1, int number2)
 {
-	int sum;
-	sum = add(number1 + number2);
-
+	int sum = number1 + number2;
 	return(sum);
 }
