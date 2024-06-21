a117c0d41bde152c96b0c5828feff5c61511b5e1
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-21 09:54:00 +0200

    C - Functions, nested loops

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add -  adds two integers and returns the result.
+ *
+ * Return: alway zero
+ */
+
+int add(int, int)
+{
+	int number1, number2, sum;
+
+	sum = number1 + number2;
+	_putchar(sum);
+
+}
