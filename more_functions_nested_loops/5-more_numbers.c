b914eca7239ca28294c83c0310fb5922a7c881d4
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-24 10:13:35 +0200

    C - More functions, more nested loops

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - a function that prints 10 times the numbers, from 0 to 14
+ *
+ * Return: alway zero
+ */
+
+void more_numbers(void)
+{
+	int n, t;
+
+	for (t = 0; n < 10; t++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			_putchar(n + '0');
+		}
+		_putchar('\n');
+	}
+}
