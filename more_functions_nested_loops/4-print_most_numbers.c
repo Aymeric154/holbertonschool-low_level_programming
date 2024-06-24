04fc78958f3850823c6a4c384fcbdeb8769d9827
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-24 09:59:34 +0200

    C - More functions, more nested loops

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9
+ * Do not print 2 and 4
+ *
+ * Return: alway zero
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if ( n == 2 && n == 4)
+		{
+			_putchar();
+		}
+		else 
+		{
+			_putchar(n + '0');
+		}
+}
