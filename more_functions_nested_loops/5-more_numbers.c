c93f069927dd8872e4f53df354aa83fffd695e13
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-24 10:37:52 +0200

    C - More functions, more nested loops

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,14 @@
 
 void more_numbers(void)
 {
-	int n, t;
+	int t, n;
 
-	for (t = 0; n < 10; t++)
+
+	for (t = 0; t <= 10; t++)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			if (n > 10)
+			if (n > 9)
 			{
 				_putchar((n / 10) + '0');
 			}
