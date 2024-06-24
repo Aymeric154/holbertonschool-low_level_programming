68f36394a1f33dac0fe10c790c86f14433ba35bf
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-24 09:45:31 +0200

    C - More functions, more nested loops

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9
+ *
+ * Return: alway zero
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n + '0');
+	}
+		
+}
