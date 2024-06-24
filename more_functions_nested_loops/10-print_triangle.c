459781a6007db252ab407ca563703473867527cd
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-24 15:43:41 +0200

    C - More functions, more nested loops

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,26 +9,23 @@
 void print_triangle(int size)
 {
 	{
-		int n, m;
+		int n, m, o;
 
 		if (size < 0)
 		{
 			_putchar('\n');
 		}
-		for (n = 1 ; n <= size ; n++)
-		{
-			for (m = 1; m < size; m++)
+			for (n = 1; n < size ; n++)
 			{
-				if (m <= size - n)
+				for (m = n; m < size; m++)
 				{
 					_putchar(32);
 				}
-				else
+				for (o = 1; o <= n; o++) 
 				{
 					_putchar(35);
 				}
+				_putchar('\n');
 			}
-			_putchar('\n');
-		}
 	}
 }
