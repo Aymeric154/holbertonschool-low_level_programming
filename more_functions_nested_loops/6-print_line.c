162a81f083e3012614b0d2f700929dd91a67784c
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-24 10:57:05 +0200

    C - More functions, more nested loops

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line in the terminal
+ *
+ * Return: always zero
+ */
+
+void print_line(int n)
+{
+	int m;
+
+	if ( n < 0)
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		for (m = 0; m < n ; m++)
+		{
+			_putchar(95);
+		}
+	_putchar('\n');
+	}
+}
