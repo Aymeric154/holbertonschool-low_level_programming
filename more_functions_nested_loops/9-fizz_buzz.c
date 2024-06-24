6f38017df6ac7c71d9a620d17b27bab527e5c261
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-24 14:10:36 +0200

    C - More functions, more nested loops

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,17 @@ int main(void)
 
 	for (number = 1; number <= 100; number++)
 	{
-		if (number % 3 == 0)
+		if (number % 3 == 0 && number % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (number % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		if (number % 5 == 0)
+		else if (number % 5 == 0)
 		{
 			printf("Buzz ");
-		}
-		else if (number % 3 == 0 && number % 5 == 0)
-		{
-			printf("FizzBuzz ");
 		}
 		else
 		{
