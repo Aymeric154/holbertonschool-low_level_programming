6cecf0fb2bfcf9a019411e6694865435504a675f
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-25 10:03:35 +0200

    C - Pointers, arrays and strings

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * a function that swaps the values of two integers
+ * swap_int - function that swaps the values of two integers
  *
  * @a: name of the variable
  * @b: name of the variable
