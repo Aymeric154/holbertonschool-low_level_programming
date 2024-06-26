bd3e4240aec00cd35c695304dd18933920a45287
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-26 15:03:33 +0200

    C - Pointers, arrays and strings

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,6 +2,8 @@
 
 /**
  * *_strcpy - unction that copies the string pointed to by src
+ * @dest: the buffer pointed
+ * @src:  copies the string pointed
  *
  * Return: the pointer to dest
  */
@@ -10,7 +12,7 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; src[i]; i++)
+	for (i = 0; src[i] != 0; i++)
 	{
 		dest[i] = src[i];
 	}
