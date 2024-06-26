e5caf06607b2ab47952fcd17afa74ba05adca90f
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-26 13:52:22 +0200

    C - Pointers, arrays and strings

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,15 +10,15 @@
 
 void puts_half(char *str)
 {
-	int i = 0, n = 0, j= 0;
+	int i = 0, n = 0, j = 0;
 
-	for ( i = 0; str[i] != 0; i++)
+	for (i = 0; str[i] != 0; i++)
 	{
 	}
 	n = i / 2;
 	if ((i % 2) != 0)
 	{
-		n = (i + 1)/ 2;
+		n = (i + 1) / 2;
 	}
 	for (j = n ; str[j] != 0; j++)
 	{
