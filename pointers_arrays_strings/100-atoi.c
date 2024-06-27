e5f02d3e0c0c6f8b167a9e3c370f361fd473f83d
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-27 13:48:42 +0200

    C - Pointers, arrays and strings

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - function that convert a string to an integer
+ *
+ * @s:
+ *
+ * Return: 0;
+ */
+
+int _atoi(char *s)
+{
+	int i = 0, n = 1, j;
+
+	while (*s)
+	{
+		if (*s == '-')
+
+			n *= -1;
+		else if	(*s >= '0' && *s <= '9')
+		{
+			j = i;
+			i = i * 10 + (*s - '0');
+
+			if (i / 10 != j)
+			{
+			
+				return (j * n);
+			}
+
+		}
+		else if (i != 0)
+			break;
+		s++;
+	}
+	return (i * n);
+}
