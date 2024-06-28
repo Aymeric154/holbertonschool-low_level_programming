3383b467a69f85852a27c81daaf65fdd9ebb7114
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-28 09:42:30 +0200

    C - More pointers, arrays and strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strcat - function that concatenates two strings
+ *
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != 0)
+	{
+		i++;
+	}
+
+	i = 0;
+
+	while (src[j] != 0)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	return (dest);
+
+}
