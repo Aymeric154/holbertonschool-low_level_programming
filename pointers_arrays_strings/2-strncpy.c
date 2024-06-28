d6e8eeb023e0c31fd7f9c15c814074a11b948eff
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-28 10:51:27 +0200

    C - More pointers, arrays and strings

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,19 +12,20 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	
+
 	int i = 0;
 
-	for(i = 0; dest[i] != 0; i++)
+	while (src[i] != 0 && i < n)
 	{
-		if (i < n)
-		{
 		dest[i] = src[i];
-		}
-		else
-		{
-		}
+		i++;
 
 	}
+	while (i < n)
+	{
+		dest[i] = 0;
+		i++;
+	}
+
 	return (dest);
 }
