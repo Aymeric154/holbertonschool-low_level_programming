3784f628dff0763798478fad730e7e765c922dfc
Aymeric154 <aymeric.gelle@outlook.fr>
2024-06-28 09:59:48 +0200

    C - More pointers, arrays and strings

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strncat
+ *
+ * @dest:
+ * @src:
+ * @n:
+ *
+ * return: 0
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != 0)
+	{
+		i++;
+	}
+
+	while (src[j] != 0 && j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	return (dest);
+
+}
