165b93c4aa27d7f44bb2f2d19f3b238f9bd5114b
Aymeric154 <aymeric.gelle@outlook.fr>
2024-07-01 16:01:30 +0200

    C - Even more pointers, arrays and strings

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -16,9 +16,7 @@ void print_chessboard(char (*a)[8])
 	{
 		for (col = 0; col < 8; col++)
 		{
-			_putchar(a[row][col]);
-			if (col < 7)
-				_putchar(' ');
+			_putchar(a[row][col];
 		}
 		_putchar('\n');
 	}
