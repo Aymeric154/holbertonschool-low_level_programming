45fda2e300c730b900667d867406de9dd4818b4d
Aymeric154 <aymeric.gelle@outlook.fr>
2024-07-01 16:00:10 +0200

    C - Even more pointers, arrays and strings

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints the chessboard.
+ *
+ * @a: two dimension array
+ *
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row = 0, col = 0;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+			if (col < 7)
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}	
