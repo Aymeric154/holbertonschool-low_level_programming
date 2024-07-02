928185424a8c7293ab9b05c925adfa7c4f6c6f4e
Aymeric154 <aymeric.gelle@outlook.fr>
2024-07-02 09:42:46 +0200

    C - Even more pointers, arrays and strings

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -20,10 +20,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		sum1 = sum1 + a[i * size + i];
-		if (i < size - 1)
-		{
-			sum2 = sum2 + a[(i + 1) * size - 1];
-		}
+		sum2 = sum2 + a[(size * (i + 1)) - (i + 1)];
 	}
 	printf("%d, %d,\n", sum1, sum2);
 
