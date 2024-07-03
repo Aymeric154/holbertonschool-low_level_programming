921c9498e617d70d25c9db4eac4d806d9ef83ba9
Aymeric154 <aymeric.gelle@outlook.fr>
2024-07-03 11:29:47 +0200

    C - Recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,38 +2,38 @@
 
 int check_prime(int n, int i);
 /**
- * is_prime_number -  function that returns 1 
- * if the input integer is a prime number, 
+ * is_prime_number -  function that returns 1
+ * if the input integer is a prime number,
  * otherwise return 0.
  *
- * @n:
+ * @n: prime number
  *
  * Return: 0 or 1
  */
 
 int is_prime_number(int n)
 {
-	return check_prime (n, 2);
+	return (check_prime(n, 2));
 }
 
 int check_prime(int n, int i)
 {
-    if (n <= 1)
-    {
-        return (0);
-    }
-    if (n == 2)
-    {
-        return (1);
-    }
-    if (n % i == 0)
-    {
-        return (0);
-    }
-    if (i * i > n)
-    {
-        return (1);
-    }
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n == 2)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	if (i * i > n)
+	{
+		return (1);
+	}
 
-    return check_prime(n, i + 1);
+	return (check_prime(n, i + 1));
 }
