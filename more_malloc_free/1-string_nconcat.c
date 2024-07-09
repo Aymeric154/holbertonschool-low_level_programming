92112056629b7bf523f8caea4699ebcb82cab9bb
Aymeric154 <aymeric.gelle@outlook.fr>
2024-07-09 10:26:37 +0200

    C - more malloc, free

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -31,12 +31,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (n >= len2)
 	{
+		n = len2;
+	}
 		s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	}
-	else
-	{
-		s3 = malloc(sizeof(char) * (len1 + n + 1));
-	}
 	if (s3 == NULL)
 	{
 		return (NULL);
