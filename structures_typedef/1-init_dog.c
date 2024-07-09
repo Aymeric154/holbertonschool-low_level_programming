e6aa2ce9dbbf50910550d7b8b3b7dd1649ce2e1b
Aymeric154 <aymeric.gelle@outlook.fr>
2024-07-09 14:09:26 +0200

    C - Structures, typedef

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ *
+ * @d: structure
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: property of dog
+ *
+ * Return: 0
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog my_dog
+	{
+		my_dog.name = "Poppy";
+		my_dog.age = 3.5;
+		my_dog.owner = "Bob";
+
+		printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+		return (0);
+	}
+}
