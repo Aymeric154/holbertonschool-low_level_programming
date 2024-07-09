4189fbe8df6c28f89aa9b015d3881761b66121ee
Aymeric154 <aymeric.gelle@outlook.fr>
2024-07-09 13:46:22 +0200

    C - Structures, typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,18 @@
-int main(void)
-{
 
-	struct dog 
-	{
-		char *name;
-		float age;
-		char *owner;
-	}
+
+
+/**
+ * struct dog - dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Description: Defines metadata about the dog and type of dog's struct
+ */
+
+typedef struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
 }
