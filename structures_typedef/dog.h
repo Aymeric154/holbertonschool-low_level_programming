0fb2b79f0805e3d8982e134bb0800c2071833b60
Aymeric154 <aymeric.gelle@outlook.fr>
2024-07-09 13:51:14 +0200

    C - Structures, typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -4,6 +4,7 @@
 
 /**
  * struct dog - dog data
+ *
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the owner of the dog
@@ -11,7 +12,7 @@
  * Description: Defines metadata about the dog and type of dog's struct
  */
 
-typedef struct dog 
+struct dog 
 {
 	char *name;
 	float age;
