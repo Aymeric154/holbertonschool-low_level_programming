ccec259974bcba5417f181b4f9947958ec8eb041
Aymeric154 <aymeric.gelle@outlook.fr>
2024-07-09 09:42:08 +0200

    C - more malloc, free

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - Write a function that allocates memory using malloc
+ * 
+ * @b: insigned int type
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *i;
+
+	i = malloc(b);
+
+	if (i == NULL)
+	{
+		exit(98);
+	}
+	return (i);
+}
