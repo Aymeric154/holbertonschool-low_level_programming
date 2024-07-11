8d52d0429f3faad7a740e546514bb547884f587f
Aymeric154 <aymeric.gelle@outlook.fr>
2024-07-11 08:43:14 +0200

    C - Structures, typedef

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dogs.
+ *
+ * @d: free dog
+ *
+ * Return: d
+ */
+
+void free_dog(dog_t *d)
+{
+    if (d == NULL)
+        return;
+
+    free(d->name);
+    free(d->owner);
+    free(d);
+}
