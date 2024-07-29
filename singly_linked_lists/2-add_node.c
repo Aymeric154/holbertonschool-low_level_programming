d14cdd05253639832d819865b9bd1e82b2e13a67
Aymeric154 <aymeric.gelle@outlook.fr>
2024-07-29 14:01:29 +0200

    fix betty coding style

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -22,7 +22,7 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		i++;
 	}
-	
+
 	if (new_node == NULL)
 	{
 		return (NULL);
@@ -31,7 +31,7 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
-		free (new_node);
+		free(new_node);
 		return (NULL);
 	}
 
