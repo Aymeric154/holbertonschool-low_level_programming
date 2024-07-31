b82c3921283136995f7d97860ac28a3ae427f39a
Aymeric154 <aymeric.gelle@outlook.fr>
2024-07-31 10:54:39 +0200

    create readme

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <stddef.h>
 
 /**
  * binary_to_uint - function that converts a binary number
@@ -13,21 +14,22 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0;
-	unsigned int j;
+	unsigned int j = 0;
 
 	if (b == NULL)
 	{
 		return (0);
 	}
 
-	for (i = 0; b[i] != 0; i++)
+	while (b[i] != '\0')
 	{
 		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
 
-		j = (j << 1) + (b[i] - '0'); 
+		j = j * 2 + (b[i] - '0');
+		i++;
 	}
 
 	return (j);
