25ba5fced71da85ac881168bb03ca635f89dc690
Aymeric154 <aymeric.gelle@outlook.fr>
2024-08-01 09:23:49 +0200

    fix problem

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -48,13 +48,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		count++;
 		current = current->next;
 	}
-
 	if (count == idx)
 	{
 		free(new_node);
 		return (add_dnodeint_end(h, n));
 	}
-
 	free(new_node);
 	return (NULL);
 }
