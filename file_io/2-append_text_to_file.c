ff7721e10ca8d3600ce2f18395b93b856edcf5bb
Aymeric154 <aymeric.gelle@outlook.fr>
2024-08-01 22:06:08 +0200

    checktask 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,7 +8,7 @@
  * append_text_to_file - function that appends text at the end of a file.
  *
  * @filename: name of the file
- * @text_content is the NULL terminated string to add at the end of the file
+ * @text_content: is the NULL terminated string to add at the end of the file
  *
  * Return: 1 on success or -1 on failure
  */
